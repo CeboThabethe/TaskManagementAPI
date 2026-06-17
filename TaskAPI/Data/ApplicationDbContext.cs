346679156f5424e57d38b938d4bb21f7a5557d0e
ST10320176 <st10320176@myemeris.edu.za>
2026-06-17 21:43:08 +0200

    Added ApplicationDbContext with EF Core

---
+++ TaskAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskAPI.Models;
+
+namespace TaskAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
